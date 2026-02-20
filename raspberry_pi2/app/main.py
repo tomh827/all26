496befc2629dcaa686ab23cfc268c5a1f98c87cf
truher <joel@truher.org>
2026-02-19 18:06:54 -0800

    queue to connect threads

--- raspberry_pi2/app/main.py
+++ raspberry_pi2/app/main.py
@@ -9,63 +9,28 @@ use the script called "runapp.py" in the raspberry_pi directory
 
 # pylint: disable=R0914
 
-import time
 import ntcore
-from app.network.network import SyncRequest, SyncReply
+from queue import Queue, Empty
 
-estimate: int = 0
-
-
-def offset(org: int, rec: int, xmt: int, dst: int) -> int:
-    return (rec + xmt - dst - org) // 2
-
-
-def fuse(measurement: int) -> None:
-    global estimate
-    diff = estimate - measurement
-    if abs(diff) > 1000000:
-        # large change, step right to the measurement
-        estimate = measurement
-    else:
-        estimate = (estimate * 99 + measurement) // 100
+from threading import Event, Thread
+from app.sync.sync_loop import SyncLoop
+from app.test.drift_loop import DriftLoop
 
 
 def main() -> None:
     print("*** main")
 
+    done = Event()
     inst = ntcore.NetworkTableInstance.getDefault()
     inst.startClient4("sync_client")
     inst.setServer("10.1.0.2")
 
-    request_pub = inst.getStructTopic("syncrequest", SyncRequest).publish()
+    queue: Queue = Queue()
 
-    reply_sub = inst.getStructTopic("syncreply", SyncReply).subscribe(
-        SyncReply(0, 0, 0)
-    )
+    syncloop = SyncLoop(inst, queue, done)
+    driftloop = DriftLoop(inst, queue, done)
 
-    estimate_pub = inst.getIntegerTopic("estimate").publish()
+    Thread(target=syncloop.run).start()
+    Thread(target=driftloop.run).start()
 
-    drift_pub = inst.getDoubleTopic("drift (us)").publish()
-
-    first_estimate = 0
-
-    while True:
-
-        request_pub.set(SyncRequest(ntcore._now()))
-
-        queue: list[ntcore.TimestampedStruct] = reply_sub.readQueue()
-        if queue:
-            syncreply: SyncReply = queue[-1].value
-            now = ntcore._now()
-            measurement = offset(syncreply.org, syncreply.rec, syncreply.xmt, now)
-            fuse(measurement)
-            estimate_pub.set(estimate)
-
-            if first_estimate == 0:
-                first_estimate = estimate
-
-            drift = estimate - first_estimate
-            drift_pub.set(drift)
-
-        # avoid spinning too fast
-        time.sleep(0.02)
+    done.wait()
