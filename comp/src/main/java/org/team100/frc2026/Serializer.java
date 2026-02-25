e9c2237284c6cf33913f7a07ee66265ff1123229
marcelosin1010 <marcelo.sin1010@gmail.com>
2026-02-24 18:39:07 -0800

    Serailizer start stop

--- comp/src/main/java/org/team100/frc2026/Serializer.java
+++ comp/src/main/java/org/team100/frc2026/Serializer.java
@@ -1,18 +1,89 @@
 package org.team100.frc2026;
 
+import org.team100.lib.config.Identity;
+import org.team100.lib.config.PIDConstants;
 import org.team100.lib.logging.LoggerFactory;
 import org.team100.lib.motor.BareMotor;
+import org.team100.lib.motor.MotorPhase;
+import org.team100.lib.motor.NeutralMode100;
+import org.team100.lib.motor.ctre.KrakenX60Motor;
 import org.team100.lib.motor.sim.SimulatedBareMotor;
+import org.team100.lib.util.CanId;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Serializer extends SubsystemBase {
     private final BareMotor m_motor;
+    private final BareMotor m_motor2;
+    private final BareMotor m_motor3;
+    private final BareMotor m_motor4;
 
-    public Serializer(LoggerFactory parent) {
+    private final double m_speed = 30;
+
+    public Serializer(LoggerFactory parent, CanId canID) {
         LoggerFactory log = parent.type(this);
-        m_motor = new SimulatedBareMotor(log, 600);
+        switch (Identity.instance) {
+            case TEST_BOARD_B0, COMP_BOT -> {
+                //
+                PIDConstants PID = PIDConstants.makeVelocityPID(log, 0.1);
+                // two is too low, even for unloaded case
+                double supplyLimit = 50;
+                double statorLimit = 20;
+                m_motor = new KrakenX60Motor(
+                        log.name("Shooter1"), // LoggerFactory parent,
+                        canID, // CanId canId,
+                        NeutralMode100.COAST, // NeutralMode neutral,
+                        MotorPhase.REVERSE, // MotorPhase motorPhase,
+                        supplyLimit, // supplyLimit,
+                        statorLimit, // statorLimit,
+                        KrakenX60Motor.highFrictionFF(log), // Feedforward100 ff
+                        KrakenX60Motor.highFriction(log),
+                        PID// PIDConstants pid,
+                );
+
+                m_motor2 = new KrakenX60Motor(
+                        log.name("Shooter2"), // LoggerFactory parent,
+                        canID, // CanId canId,
+                        NeutralMode100.COAST, // NeutralMode neutral,
+                        MotorPhase.REVERSE, // MotorPhase motorPhase,
+                        supplyLimit, // supplyLimit,
+                        statorLimit, // statorLimit,
+                        KrakenX60Motor.highFrictionFF(log), // Feedforward100 ff
+                        KrakenX60Motor.highFriction(log),
+                        PID// PIDConstants pid,
+                );
+                m_motor3 = new KrakenX60Motor(
+                        log.name("Shooter3"), // LoggerFactory parent,
+                        canID, // CanId canId,
+                        NeutralMode100.COAST, // NeutralMode neutral,
+                        MotorPhase.REVERSE, // MotorPhase motorPhase,
+                        supplyLimit, // supplyLimit,
+                        statorLimit, // statorLimit,
+                        KrakenX60Motor.highFrictionFF(log), // Feedforward100 ff
+                        KrakenX60Motor.highFriction(log),
+                        PID// PIDConstants pid,
+                );
+                m_motor4 = new KrakenX60Motor(
+                        log.name("Shooter3"), // LoggerFactory parent,
+                        canID, // CanId canId,
+                        NeutralMode100.COAST, // NeutralMode neutral,
+                        MotorPhase.REVERSE, // MotorPhase motorPhase,
+                        supplyLimit, // supplyLimit,
+                        statorLimit, // statorLimit,
+                        KrakenX60Motor.highFrictionFF(log), // Feedforward100 ff
+                        KrakenX60Motor.highFriction(log),
+                        PID// PIDConstants pid,
+                );
+            }
+            default -> {
+                m_motor = new SimulatedBareMotor(log.name("Shootmotor1"), 600);
+                m_motor2 = new SimulatedBareMotor(log.name("Shootmotor2"), 600);
+                m_motor3 = new SimulatedBareMotor(log.name("Shootmotor3"), 600);
+                m_motor4 = new SimulatedBareMotor(log.name("Shootmotor3"), 600);
+
+            }
+        }
     }
 
     @Override
@@ -21,7 +92,7 @@ public class Serializer extends SubsystemBase {
     }
 
     public Command serialize() {
-        return run(this::fullSpeed);
+        return run(this::fullSpeed).withName("Run Serializer");
     }
 
     public Command stop() {
@@ -29,11 +100,22 @@ public class Serializer extends SubsystemBase {
     }
 
     public void stopMotor() {
-        m_motor.setDutyCycle(0);
+        m_motor.stop();
     }
 
     private void fullSpeed() {
-        m_motor.setDutyCycle(1);
+        double Velocity = 450;
+        m_motor.setVelocity(Velocity, 0, 0);
+        m_motor2.setVelocity(Velocity, 0, 0);
+        m_motor3.setVelocity(Velocity, 0, 0);
+        m_motor4.setVelocity(Velocity, 0, 0);
+    }
+
+    public void setSpeed(double Velocity) {
+        m_motor.setVelocity(Velocity, 0, 0);
+        m_motor2.setVelocity(Velocity, 0, 0);
+        m_motor3.setVelocity(Velocity, 0, 0);
+        m_motor4.setVelocity(Velocity, 0, 0);
     }
 
 }
