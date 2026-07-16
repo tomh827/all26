2caa856af7524cdb3254cce0b1bd162013d6325e
truher <joel@truher.org>
2026-07-16 13:06:29 -0700

    more camera fixes

--- raspberry_pi/app/camera/camera_factory.py
+++ raspberry_pi/app/camera/camera_factory.py
@@ -11,16 +11,25 @@ class CameraFactory:
     def get(identity: Identity) -> Camera:
         print("\n*** CameraFactory selecting a Camera", flush=True)
         try:
-            # import will fail if we're not running on a Raspberry Pi.
+            # ImportError if we're not running on a Raspberry Pi.
             from app.camera.real_camera import RealCamera
 
+            # IndexError if no camera is attached.
             return RealCamera(identity)
 
         except ImportError:
             from app.camera.fake_camera import FakeCamera
 
+            # These images can only be read from the filesystem,
+            # not from the zip file (without more work), so they're
+            # not even included in the zip file (see build.gradle)
             # 1/4 scale
             # return FakeCamera("images/tag_and_board.jpg", (1100, 620), -5)
             # full-size (huge)
             return FakeCamera("images/tag_and_board.jpg", (5504, 3096), -0.1)
             # return FakeCamera("images/blob.jpg")
+
+        except IndexError:
+            from app.camera.no_camera import NoCamera
+
+            return NoCamera()
